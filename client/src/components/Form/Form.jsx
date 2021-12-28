1e923d28dfa12357d7e91168abdb05391c9e789f
Leo <leo.cabral1016@gmail.com>
2021-12-28 16:50:52 -0300

    redux & form 1

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import useStyles from "./styles.js";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
   const classes = useStyles();
+  const handleSubmit = () => {};
 
-  return <h1>FORM</h1>;
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        ></TextField>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
